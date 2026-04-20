38c700a90338bef0d4c40ebbc5cd16e070ea74e8
OwnderDuck <ownderduck@gmail.com>
2026-04-20 14:55:00 +0800

    add header

--- extension.js
+++ extension.js
@@ -1,4 +1,11 @@
-'use strict';
+/*
+ * Copyright (c) 2026 OwnderDuck ownderduck@gmail.com
+ * * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ * * See the LICENSE file in the repository root for full license text.
+ */
+ 'use strict';
 
 import Meta from 'gi://Meta';
 import Shell from 'gi://Shell';
