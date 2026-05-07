95d08073922cd2bfb6f1aec7a234d6b8e7815a11
OwnderDuck <ownderduck@gmail.com>
2026-05-07 17:09:59 +0800

    refactor

--- extension.js
+++ extension.js
@@ -5,13 +5,13 @@
  * the Free Software Foundation, either version 3 of the License.
  * * See the LICENSE file in the repository root for full license text.
  */
- 'use strict';
+'use strict';
 // noinspection Unresolved file reference
 import Gio from 'gi://Gio';
 import Meta from 'gi://Meta';
 import Shell from 'gi://Shell';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
 
 export default class WindowRotateExtension extends Extension {
     enable() {
@@ -32,14 +32,12 @@ export default class WindowRotateExtension extends Extension {
             this._resetRotation.bind(this)
         );
     }
-
     _toggleRotation() {
         // if rotating, stop
         if (this._rotateTimer) {
             this._stopRotation();
             return;
         }
-
         // prepare to rotate
         const focusWin = global.display.focus_window;
         if (!focusWin) return;
@@ -49,16 +47,14 @@ export default class WindowRotateExtension extends Extension {
 
         actor.set_pivot_point(0.5, 0.5);
         this._rotatingActor = actor;
-        
-        const rotateIcon = new Gio.ThemedIcon({ name: 'object-rotate-right-symbolic' });
-        
+        const rotateIcon = new Gio.ThemedIcon({name: 'object-rotate-right-symbolic'});
         const nMonitors = global.display.get_n_monitors() || 1;
-        const levels = Array.from({ length: nMonitors }, () => ({
+        const levels = Array.from({length: nMonitors}, () => ({
             level: 0,
             maxLevel: 1.0
         }));
         this._lastLabel = "";
-        
+
         // start to rotate
         this._rotateTimer = setInterval(() => {
             if (!this._rotatingActor || this._rotatingActor.is_destroyed()) {
@@ -69,7 +65,6 @@ export default class WindowRotateExtension extends Extension {
             const rect = this._rotatingActor.get_transformed_extents();
             const centerX = rect.origin.x + (rect.size.width / 2);
             const centerY = rect.origin.y + (rect.size.height / 2);
-
             const dx = mouseX - centerX;
             const dy = mouseY - centerY;
             let angleDeg = Math.atan2(dy, dx) * 180 / Math.PI;
@@ -78,7 +73,6 @@ export default class WindowRotateExtension extends Extension {
             // snap
             const snapThreshold = 1;
             const snapPoints = [0, 90, 180, 270, -90, -180];
-            
             for (let snap of snapPoints) {
                 if (Math.abs(angleDeg - snap) < snapThreshold) {
                     angleDeg = snap;
@@ -86,7 +80,6 @@ export default class WindowRotateExtension extends Extension {
                 }
             }
             this._rotatingActor.rotation_angle_z = angleDeg;
-
             let displayAngle = -angleDeg;
             const currentLabel = `${displayAngle.toFixed(1)}°`;
             if (currentLabel !== this._lastLabel) {
@@ -98,36 +91,29 @@ export default class WindowRotateExtension extends Extension {
                 this._lastLabel = currentLabel;
             }
         }, 16);
-
         // if unfocused, stop
         if (this._focusId === 0) {
             this._focusId = global.display.connect('notify::focus-window', () => this._stopRotation());
         }
-        
         // click, stop
         this._clickId = global.stage.connect('button-press-event', () => this._stopRotation());
     }
-
     _stopRotation() {
         if (this._rotateTimer) {
             clearInterval(this._rotateTimer);
             this._rotateTimer = null;
         }
-
         if (this._focusId !== 0) {
             global.display.disconnect(this._focusId);
             this._focusId = 0;
         }
-
         if (this._clickId) {
             global.stage.disconnect(this._clickId);
             this._clickId = null;
         }
-
         this._rotatingActor = null;
     }
-
-     _resetRotation() {
+    _resetRotation() {
         this._stopRotation();
 
         const focusWin = global.display.focus_window;
@@ -137,20 +123,19 @@ export default class WindowRotateExtension extends Extension {
         if (!actor) return;
 
         actor.rotation_angle_z = 0;
-        const rotateIcon = new Gio.ThemedIcon({ name: 'object-rotate-right-symbolic' });
-        const levels = global.display.get_n_monitors() > 0 
+        const rotateIcon = new Gio.ThemedIcon({name: 'object-rotate-right-symbolic'});
+        const levels = global.display.get_n_monitors() > 0
             ? Array(global.display.get_n_monitors()).fill({
                 level: 0.5,
                 maxLevel: 1.0
-              })
-            : [{ level: 0.5, maxLevel: 1.0 }];
+            })
+            : [{level: 0.5, maxLevel: 1.0}];
         Main.osdWindowManager.show(
-            rotateIcon, 
+            rotateIcon,
             `0.0°`,
             levels
         );
     }
-
     disable() {
         this._stopRotation();
         Main.wm.removeKeybinding('rotate-window-press');
